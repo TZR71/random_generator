cecd3a81738ddd2c76547738b1c15e90da17e2b5
TZR71 <Tiinarojola@gmail.com>
2024-09-22 16:36:47 +0200

    Added basic form handling, added Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateQuote(event) {
+  event.preventDefault();
 
+  new Typewriter("#quote", {
+    strings: "Generating a new quote for you",
+    autoStart: true,
+    cursor: "",
+    delay: 1.5,
+  });
+}
+
+let quoteFormElement = document.querySelector("#quote-generator-form");
+quoteFormElement.addEventListener("submit", generateQuote);
