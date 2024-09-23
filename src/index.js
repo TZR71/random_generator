dd970282a5ccf71d90cfcc035b4d1a8e5359f341
TZR71 <Tiinarojola@gmail.com>
2024-09-23 13:11:27 +0200

    Added notes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+// shows the quote with "typewriter" feature
+
 function displayQuote(response) {
   new Typewriter("#quote", {
     strings: response.data.answer,
@@ -18,15 +20,15 @@ function generateQuote(event) {
     "You are a quote expert and native in Finnish language. Your mission is to create a short quote and also translate it to English. Please separate the quote and translation with a <br />. Please sign the quote `SheCodes AI`with font-size:12px,  to the bottom RIGHT corner of quote. ";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  //When quote is generated, this moves "hidden" property from class quote,
+  //When quote is generated, this moves "hidden" property from class "quote",
   //showing a message while quote is generated
   let quoteElement = document.querySelector("#quote");
   quoteElement.classList.remove("hidden");
   quoteElement.innerHTML = `<div class="composing"> Generating quote for you about "${instructionsInput.value}"...</div>`;
 
-  //Make a call ti the API
-  axios.get(apiUrl).then(displayQuote);
+  //Make a call to the API
   //Display the generated quote
+  axios.get(apiUrl).then(displayQuote);
 }
 
 let quoteFormElement = document.querySelector("#quote-generator-form");
